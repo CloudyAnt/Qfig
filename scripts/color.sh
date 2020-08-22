55be6be7de28b251e43c75261120ff64dd740d2e
chaijiaqi <itscloudy@foxmail.com>
2020-08-22 16:41:17 +0800

    chai [n/a] fix: correct colors.js

--- scripts/color.sh
+++ scripts/color.sh
@@ -4,12 +4,13 @@
 echo "echo \"\\\e[38;05;\${n}m\"hello"
 echo 'n='
 
-js=`echo 1 8 | awk '{ for(i = $1; i <= $2; i++) printf i " " }'`
-is=`echo 1 32 | awk '{ for(i = $1; i <= $2; i++) printf i " " }'`
+js=`echo 0 7 | awk '{ for(i = $1; i <= $2; i++) printf i " " }'`
+is=`echo 0 31 | awk '{ for(i = $1; i <= $2; i++) printf i " " }'`
 
 for i in $is; do
+    base=`expr $i \* 8`
     for j in $js; do
-        n=`expr $i \* $j`
+        n=`expr $base + $j`
         printf "\e[38;05;${n}m%-6s" "($n)"
     done
     echo
