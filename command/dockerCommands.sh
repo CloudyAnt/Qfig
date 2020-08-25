9c12770ad6bc32d57f61be2253a60474eb3b9e92
chaijiaqi <jiaqi.chai@olachat.im>
2020-08-25 10:10:59 +0800

    chaijiaqi [N/A] feat: add some commands to dockerCommands.sh

--- command/dockerCommands.sh
+++ command/dockerCommands.sh
@@ -1,8 +1,18 @@
+alias dstart='docker start'
 alias dstop='docker stop'
 alias dlog='docker logs --tail 200 --follow --timestamps'
-alias dps="docker ps | awk 'BEGIN{lines=-1;} {lines++; print $0} END{print "Total " lines " containers";}'"
+alias dps='docker ps'
+alias dpsa='docker ps -a'
+#alias dps="docker ps | awk 'BEGIN{lines=-1;} {lines++; print $0} END{print \"Total \" lines \" containers\";}'"
+#alias dpsa="docker ps -a | awk 'BEGIN{lines=-1;} {lines++; print $0} END{print \"Total \" lines \" containers\";}'"
+alias dis="docker images"
 
 function dbash() {
-    [ -z "$1" ] && return
     docker exec -it $1 bash
+}
+
+
+function dcrm() {
+    [ -z "$1" ] && return
+    docker container rm $1
 }
