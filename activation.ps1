56de78e86d9907c0b6a1dbc59301ba6295962267
chaijiaqi <itscloudy@foxmail.com>
2021-07-29 18:45:22 +0800

    Chai [N/A] fix: add activation.sh & activation.ps1 to repo

---
+++ activation.ps1
@@ -0,0 +1,27 @@
+# Make sure you are using Windows os! Else if you are using unix-like os, please use activation.sh
+
+$CurrentLoc = $PSScriptRoot
+
+If (-Not(Test-Path $profile)) {
+	NI $profile -Force
+}
+
+
+$activationSegment = ". $CurrentLoc/config.ps1"
+$activated = $false
+cat $profile | % {
+	If ($_.Equals($activationSegment)) {
+		$activated = $true
+		Break
+	}
+}	
+
+If ($activated) {
+	echo "Qfig had already been activated!"
+} Else {
+	$activationSegment > $profile
+	echo "Qfig has been activated! Open a new session to check."
+}
+
+clv activationSegment 
+clv activated 
---
+++ activation.sh
@@ -0,0 +1,20 @@
+# Make sure you are using unix-like os! Else if you are using Windows os, please use activation.ps1
+
+currentLoc="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+baseConfig=$currentLoc/config.sh
+# Check if actived
+
+sysConfigFile=".zshrc"
+[ ! -z "$sysConfigFile" ] && sysConifgFile=".bash_profile"
+
+activationSegment=`cat ${HOME}/.zshrc | awk -v f="$baseConfig" '$0 ~ f'`
+[ ! -z "$activationSegment" ] && echo "Qfig had already been activated!" && exit
+
+echo source $baseConfig >> $HOME/.zshrc 
+echo "Qfig has been activated! Please open a new session to check."
+
+unset currentLoc
+unset baseConfig 
+unset activationSegment 
+unset sysConfigFile
