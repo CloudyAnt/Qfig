205e90c213a477a9a71dd6e10e4b424744f47396
chaijiaqi <itscloudy@foxmail.com>
2021-07-30 10:59:21 +0800

    Chai [N/A] refactor: mv color.sh & qs.sh to /script. + color.ps1 & qs.ps1

---
+++ script/qs.ps1
@@ -0,0 +1,41 @@
+[int[]]$list = (Read-Host "Please Input an int array, separated by ','`n").split(",") | % {
+    [int]$_
+}
+
+function partition {
+    param(
+        [Parameter(Mandatory)][int[]]$list,
+        [Parameter(Mandatory)][int]$l,
+        [Parameter(Mandatory)][int]$r
+    )
+    
+    $i = $l - 1;
+    For($j = $l; $j -Lt $r; $j++ ) {
+        if ($list[$j] -Lt $list[$r]) {
+            $temp = $list[$j]
+            $list[$j] = $list[++$i]
+            $list[$i] = $temp
+        }
+    }
+    $temp = $list[$r]
+    $list[$r] = $list[++$i]
+    $list[$i] = $temp 
+    Return $i
+}
+
+function qs {
+    param(
+        [Parameter(Mandatory)][int[]]$list,
+        [Parameter(Mandatory)][int]$l,
+        [Parameter(Mandatory)][int]$r
+    )
+
+    if ($l -Lt $r) {
+        $middle = partition $list $l $r
+        qs $list $l ($middle - 1)
+        qs $list $($middle + 1) $r
+    }
+}
+
+qs $list 0 ($list.Length - 1)
+$list -join "," 
