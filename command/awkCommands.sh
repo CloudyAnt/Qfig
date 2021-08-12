bfff716cbf017de8ad458df6f82202f12d75f76c
chaijiaqi <itscloudy@foxmail.com>
2021-08-12 18:12:13 +0800

    Chai [N/A] feat: add mysqlc(connect mysql) command

--- command/awkCommands.sh
+++
@@ -1,3 +0,0 @@
-# Awk related commands.
-
-
--- command/devCommands.sh
+++ command/devCommands.sh
@@ -39,8 +39,7 @@ function mdhl() { #? hightlight a word in dependency tree
 
 ### Gradle
 
-function gpkg() {
-    logInfo "Packaging.."
+function gpkg() { logInfo "Packaging.."
     [ "-s" = $1 ] && gradle clean build -x tset || gradle clean build
     logInfo "Target size: "
     du -h target/*.jar
@@ -54,6 +53,25 @@ function gdhl() { #? hightlight a word in dependency tree
 ### MySQL
 
 alias mysqlu='mysql -uroot -p'
+
+_MYSQL_MAPPING_FILE=$Qfig_loc/mysqlMappingFile
+
+[ ! -f $_MYSQL_MAPPING_FILE ] && touch $_MYSQL_MAPPING_FILE
+eval `cat $_MYSQL_MAPPING_FILE | awk -F '=' 'BEGIN{ s0 = "";s = "declare -A _MYSQL_MAPPING=(";s1 = ""} \
+    { if ( NF >= 2) { \
+        split($2, parts, "#"); s0 = s0 ";_MYSQL_MAPPING_" $1 "=(\"" parts[1] "\" \"" parts[2] "\" \"" parts[3] "\")"; \
+        s = s " [" $1 "]=$_MYSQL_MAPPING_" $1; \
+        s1 = s1 ";unset _MYSQL_MAPPING_" $1; \
+    }} \
+    END { s = s ")"; print s0; print s; print s1}'`
+
+function mysqlc() {
+    [ -z $1 ] || [ -z $_MYSQL_MAPPING[$1] ] && logError "Which mapping?" && return
+    unset mapping
+    eval "mapping=($_MYSQL_MAPPING[$1])"
+    mysql -u $mapping[2] -p$mapping[3] -h $mapping[1]
+}
+ 
 
 ### JAVA
 
