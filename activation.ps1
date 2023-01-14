869592d4b06ffa073a474a29134c25e45c10520d
CloudyAnt <itscloudy@foxmail.com>
2023-01-14 12:10:05 +0800

    Chai-Ausu-PS [N/A] fix: Powershell activation.ps1

--- activation.ps1
+++ activation.ps1
@@ -20,13 +20,13 @@ Get-Content $profile | % {
 If ($activated) {
 	Write-Host "Qfig had already been activated!"
 } Else {
-	$activationSegment > $profile
+	$activationSegment >> $profile
 	Write-Host "Qfig has been activated! Open a new session to check."
 }
 
 # Delete old registration
-$oldSegment = ". $CurrentLoc/config.ps1"
-Set-Content -Path $profile -Value (Get-Content -Path $profile | Select-String -Pattern "$oldSegment" -NotMatch)
+$oldSegment = "\. $CurrentLoc/config.ps1"
+Set-Content -Path $profile -Value (Get-Content -Path $profile | Select-String -Pattern "$oldSegment" -SimpleMatch -NotMatch)
 
 clv activationSegment 
 clv activated 
