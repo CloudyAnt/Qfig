166fd480b78f7f5a1b24a4980ff2cbf6a342ced3
Ant <iiim.cloudy@gmail.com>
2023-01-18 16:40:36 +0800

    15Mac [0] feat: show verbose init msg

--- init.sh
+++ init.sh
@@ -3,20 +3,24 @@
 ## The location of Qfig project
 Qfig_loc=$(dirname "$0")
 
-## The functions used in this script
-function doNothing() {}
-
 ## Base configs
 source $Qfig_loc/command/baseCommands.sh
 
 ## Custom configs 
+[[ "true" = $(sed -rn 's|<showVerboseInitMsg>(.+)</showVerboseInitMsg>|\1|p' $Qfig_loc/config) ]] && verbose=1
+
 preferTextEditor=vim
 if [ -f "$Qfig_loc/config" ]
 then
     awk '/<enabledCommands>/{f = 1; next} /<\/enabledCommands>/{f = 0} f' $Qfig_loc/config | \
         while read -r cmds; do \
             cmdsFile="$Qfig_loc/command/${cmds}Commands.sh"
-            [ -f "$cmdsFile" ] && source $cmdsFile || logWarn "$cmdsFile Not Exists!"
+			if [ -f "$cmdsFile" ]; then
+				source $cmdsFile
+				[ $verbose ] && logInfo "Enabled commands: $cmds"
+			else
+				logWarn "$cmdsFile Not Exists!"
+			fi
         done
     
     _preferTextEditor=$(sed -rn 's|<preferTextEditor>(.+)</preferTextEditor>|\1|p' $Qfig_loc/config)
@@ -24,12 +28,14 @@ then
     then
         preferTextEditor=$_preferTextEditor
         unset _preferTextEditor
-        logInfo "Using prefer text editor: $preferTextEditor"
+        [ $verbose ] && logInfo "Using prefer text editor: $preferTextEditor"
+	elif [ $verbose ]; then
+		logInfo "Using default text editor: $preferTextEditor"
     fi
 fi
 
 ## For functions only works on current computer, add them to the tempCommands.sh/tempCommands.ps1
 ## All functions in these files will not be included in git
-[ -f "$Qfig_loc/command/tempCommands.sh" ] && source $Qfig_loc/command/tempCommands.sh || doNothing 
+[ -f "$Qfig_loc/command/tempCommands.sh" ] && source $Qfig_loc/command/tempCommands.sh
 
-unset -f doNothing
+unset verbose
