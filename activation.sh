888436b10cd678ad06d5ebe864338ddf2ed9fa89
cloudy <iiim.cloudy@gmail.com>
2024-06-05 09:31:48 +0800

    Chai-Silicon refactor: activation.sh

--- activation.sh
+++ activation.sh
@@ -1,53 +1,27 @@
 # Activate Qfig for zsh(or bash). Please use activation-cygwin.sh if under cygwin
-currentShell=$(ps -p $$ -o comm= 2>/dev/null) # if [[ "$OSTYPE" == "darwin"* ]] || [[ "$OSTYPE" == "linux"* ]]
-if [ $? -ne 0 ]; then
-	currentShell=$(ps -p $$ comm= 2>/dev/null) # elif [[ "$OSTYPE" == "msys" ]] || [[ "$OSTYPE" == "cygwin" ]]
-fi
-if [ $? -ne 0 ]; then
-	echo "Sorry, cannot determine the current shell."
-	exit 1
-fi
-
-if [[ "$currentShell" =~ ^.*zsh$ ]]; then
-	sysConfigFile="$HOME/.zshrc" # .zshrc will always be loaded
-elif [[ "$currentShell" =~ ^.*bash$ ]]; then
-	sysConfigFile="$HOME/.bashrc" # bash only run .bashrc if not login shell
-	sysConfigFile1="$HOME/.bash_profile" # bash only run .bash_profile if login shell
-else
-	echo "Sorry, only zsh and bash are supported now."
-	unset currentShell
-	exit 1
-fi
-
-[ ! -f "$sysConfigFile" ] && touch $sysConfigFile || :
-[[ ! -z "$sysConfigFile1" && ! -f "$sysConfigFile1" ]] && touch $sysConfigFile1 || :
 
 currentLoc=$( cd $( dirname ${BASH_SOURCE[0]} ) >/dev/null 2>&1 && pwd )
 baseConfig=$currentLoc/init.sh
+function activeQfigForProfile() {
+    local profile=$1
+    
+    # Check if actived
+    activationSegment=$(cat $profile | awk -v f="$baseConfig" '$0 ~ f')
 
-# Check if actived
-activationSegment=$(cat $sysConfigFile | awk -v f="$baseConfig" '$0 ~ f')
-if [ ! -z "$activationSegment" ]
-then
-	echo "Qfig had already been activated!"
+    if [ -z "$activationSegment" ]; then
+        # Add registration
+        echo source $baseConfig >> $profile
+        activation=1
+    fi
+}
+
+profiles=($HOME/.zshrc $HOME/.bashrc $HOME/.bash_profile)
+for profile in "${profiles[@]}";do
+    activeQfigForProfile $profile
+done
+
+if [ "$activation" ]; then
+    echo "Qfig has been activated(for zsh and bash)! Please open a new session to check."
 else
-	# Add registration
-	echo source $baseConfig >> $sysConfigFile
-
-	# Add registration to the 2nd profile if not added
-	if [ ! -z "$sysConfigFile1" ]; then
-		activationSegment=$(cat $sysConfigFile1 | awk -v f="$baseConfig" '$0 ~ f')
-		[ -z "$activationSegment" ] && echo source $baseConfig >> $sysConfigFile1 || :
-	fi
-	echo "Qfig has been activated! Please open a new session to check."
+    echo "Qfig had already been activated(for zsh and bash)! Please open a new session to check."
 fi
-
-# Init
-source $sysConfigFile
-
-unset currentShell
-unset currentLoc
-unset baseConfig 
-unset activationSegment 
-unset sysConfigFile
-unset sysConfigFile1
