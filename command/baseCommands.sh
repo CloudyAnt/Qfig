c314c65895ab1b10d191c531128ed9c6e460f47c
Cloudy <iiim.cloudy@gmail.com>
2024-07-23 11:22:58 +0800

    Chai-M3MBP refactor: extract some awk script into files

--- command/baseCommands.sh
+++ command/baseCommands.sh
@@ -167,48 +167,7 @@ function qcmds() { #? operate available commands. Usage: qcmds $commandsPrefix $
 			editfile $targetFile && return
 			;;
 		""|explain)
-			cat $targetFile | awk '{
-					if (/^#\? /) {
-						printf "\033[34m▍\033[39m";
-						for (i = 2; i <= NF; i++) {
-							printf $i " ";
-						}
-						printf "\033[0m\n";
-					} else if (/^function /) {
-						if ($4 == "#x") next;
-						command = "\033[34m" $2 "\033[2m ";
-						while(length(command) < 29) {
-							command = command "-";
-						}
-						printf("%s\033[0m", command);
-						if ($4 == "#?") {
-							printf "\033[36m ";
-						} else if ($4 == "#!") {
-							printf "\033[31m ";
-						} else {
-							printf " ";
-						}
-						for (i = 5; i <= NF; i++) {
-							printf $i " ";
-						}
-						printf "\033[0m\n";
-					} else if (/^alias /) {
-						# gsub("'\''", "", $2);
-						split($2, parts, "=");
-						printf "\033[32malias \033[34m" parts[1] "\033[39m = \033[36m" parts[2];
-						for (i = 3; i <= NF; i++) {
-							# gsub("'\''", "", $i);
-							printf(" %s", $i);
-						}
-						printf "\033[0m\n";
-					} else if (/^forbidAlias /) {
-						printf "\033[32malias \033[31m" $2 " \033[39m=>\033[34m";
-						for (i = 3; i <= NF; i++) {
-							printf(" %s", $i);
-						}
-						printf "\033[0m\n";
-					}
-				}' | awk '{sub("\\(\\)", "\033[37m()\033[0m")} 1'
+			awk -f "$_QFIG_LOC/staff/explainCommands.awk" "$targetFile" | awk '{sub("\\(\\)", "\033[37m()\033[0m")} 1'
 			return
 			;;
 		*)
