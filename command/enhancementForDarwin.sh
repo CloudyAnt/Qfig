f09aa1a88a94f943e69661382e687d904201a7d6
Cloudy <iiim.cloudy@gmail.com>
2024-08-03 12:22:45 +0800

    Chai-M3MBP feat: MacOs brew services alias

--- command/enhancementForDarwin.sh
+++ command/enhancementForDarwin.sh
@@ -1,6 +1,11 @@
 # Enhancements for Darwin (macOS)
 
-function dquarantine() {  # delete quarantine attr
+function dquarantine() {  #? delete quarantine attr
   [ -z "$1" ] && logError "Specify the path!" || :
   xattr -d com.apple.quarantine $1;
 }
+
+alias bstart="brew services start"
+alias bstop="brew services stop"
+alias brestart="brew services restart"
+alias bstatus="brew services list"
