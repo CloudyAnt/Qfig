39022dcb4d720781dcc2d6f15fe1197d0a896a4b
Cloudy <iiim.cloudy@gmail.com>
2025-03-03 10:45:00 +0800

    Chai-M3MBP refactor: hslv2rgb -> hsl,hsv & rgbPct

--- command/colorCommands.sh
+++ command/colorCommands.sh
@@ -1,6 +1,22 @@
 #? commands to operate colors
-function rgb() { #? show rgb color(rgb values or hexadecimal color value). Usage: rgb 100 200 255; rgb 64c8ff
+
+function rgb() { #? show rgb color(rgb values or hexadecimal color value). Usage: rgb 100 200 255; rgb 64c8ff;
     local r g b hex
+    local noHsl noHsv noPct
+    OPTIND=1
+    while getopts "hlvp" opt; do
+        case $opt in
+            h)
+              logInfo "Usage: rgb \$red \$green \$blue."
+              logInfo "Usage: rgb \$hexadecimal."
+              return 0;;
+            l) noHsl=1;;
+            v) noHsv=1;;
+            p) noPct=1;;
+            \?) logError "Invalid option: -$OPTARG"; return 1;;
+        esac
+    done
+    shift "$((OPTIND - 1))"
 
     # Handle RGB decimal values
     if [[ "$1" =~ ^[0-9]+$ && "$2" =~ ^[0-9]+$ && "$3" =~ ^[0-9]+$ ]]; then
@@ -23,12 +39,18 @@ function rgb() { #? show rgb color(rgb values or hexadecimal color value). Usage
         hex=$(printf "%02x%02x%02x" "$r" "$g" "$b")
     # Handle hex color value
     elif [[ $1 =~ ^[0-9a-fA-F]{6}$ ]]; then
-        r=$(+convertHexColorUnit2Dec ${1:0:2})
-        g=$(+convertHexColorUnit2Dec ${1:2:2})
-        b=$(+convertHexColorUnit2Dec ${1:4:2})
         hex=$1
+        r=$(+convertHexColorUnit2Dec ${hex:0:2})
+        g=$(+convertHexColorUnit2Dec ${hex:2:2})
+        b=$(+convertHexColorUnit2Dec ${hex:4:2})
+    # Handle short hex color value
+    elif [[ $1 =~ ^[0-9a-fA-F]{3}$ ]]; then
+        r=$(+convertHexColorUnit2Dec "${1:0:1}${1:0:1}")
+        g=$(+convertHexColorUnit2Dec "${1:1:1}${1:1:1}")
+        b=$(+convertHexColorUnit2Dec "${1:2:1}${1:2:1}")
+        hex=$(printf "%02x%02x%02x" "$r" "$g" "$b")
     else
-        logError "Please specify r, g, b values like '100 200 255' or hex color name like '64c8ff'" && return 1
+        logError "Invalid input ! Valid input examples: 100 200 255; 64c8ff; 6cf" && return 1
     fi
 
     # Check terminal support
@@ -39,81 +61,32 @@ function rgb() { #? show rgb color(rgb values or hexadecimal color value). Usage
     printf "\e[48;2;%d;%d;%dm  \e[0m\n" "$r" "$g" "$b"
 
     printf "rgb: $r $g $b\n"
-    printf "hex: $hex\n"
+    printf "hex: #$hex\n"
+
     # Convert to percentage
-    local rP=$(echo "scale=4; $r / 255 * 100" | bc | sed '/\./ s/\.\{0,1\}0\{1,\}$//')
-    local gP=$(echo "scale=4; $g / 255 * 100" | bc | sed '/\./ s/\.\{0,1\}0\{1,\}$//')
-    local bP=$(echo "scale=4; $b / 255 * 100" | bc | sed '/\./ s/\.\{0,1\}0\{1,\}$//')
-    printf "pct: $rP%% $gP%% $bP%%\n"
+    if [ -z "$noPct" ]; then
+        local rP=$(echo "scale=4; $r / 255 * 100" | bc | sed '/\./ s/\.\{0,1\}0\{1,\}$//')
+        local gP=$(echo "scale=4; $g / 255 * 100" | bc | sed '/\./ s/\.\{0,1\}0\{1,\}$//')
+        local bP=$(echo "scale=4; $b / 255 * 100" | bc | sed '/\./ s/\.\{0,1\}0\{1,\}$//')
+        printf "pct: $rP%% $gP%% $bP%%\n"
+    fi
 
     # Convert to hsl & hsv
-    local hsl=$(echo "$r $g $b" "hsl" | awk -f "$_QFIG_LOC/staff/rgb2hslv.awk")
-    local hsv=$(echo "$r $g $b" "hsv" | awk -f "$_QFIG_LOC/staff/rgb2hslv.awk")
-    printf "hsl: $hsl\n"
-    printf "hsv: $hsv\n"
+    if [ -z "$noHsl" ]; then
+        local hsl=$(echo "$r $g $b" "hsl" | awk -f "$_QFIG_LOC/staff/rgb2hslv.awk")
+        printf "hsl: $hsl\n"
+    fi
+    if [ -z "$noHsv" ]; then
+        local hsv=$(echo "$r $g $b" "hsv" | awk -f "$_QFIG_LOC/staff/rgb2hslv.awk")
+        printf "hsv: $hsv\n"
+    fi
 }
 
-function rgb2hslv() { #? convert RGB(integers) to HSL(floats) or HSV(floats). -s to show the rgb color
-    local show value
+function +convertHslv2Rgb() { #x
+    local value
     OPTIND=1
-    while getopts "hsv" opt; do
+    while getopts "v" opt; do
         case $opt in
-            h)
-              logInfo "Usage: rgb2hslv \$r \$g \$b.\n  \nFlags:\n"
-              printf "    %-5s%s\n" "h" "Print this help message"
-              printf "    %-5s%s\n" "s" "Show the color in a 24-bit color terminal" 
-              printf "    %-5s%s\n" "v" "Specify the output to HSV"
-              printf "\e[0m\n"
-              return 0;;
-            s) show=1;;
-            v) value=1;;
-            \?) logError "Invalid option: -$OPTARG"; return 1;;
-        esac
-    done
-    shift "$((OPTIND - 1))"
-
-    local r=$1 g=$2 b=$3
-
-    # Validate RGB values
-    local validation=$(awk -v r="$r" -v g="$g" -v b="$b" '
-        BEGIN {
-            err = ""
-            if (!(r ~ /^[0-9]+$/ && r >= 0 && r <= 255)) err = err "Red(0~255) "
-            if (!(g ~ /^[0-9]+$/ && g >= 0 && g <= 255)) err = err "Green(0~255) "
-            if (!(b ~ /^[0-9]+$/ && b >= 0 && b <= 255)) err = err "Blue(0~255)"
-            print err
-        }
-    ')
-
-    if [ -n "$validation" ]; then
-        logError "${validation% } is(are) invalid !" && return 1
-    fi
-
-    # Check terminal support if showing color
-    if [ "$show" ]; then
-        [[ "$COLORTERM" = "truecolor" || "$COLORTERM" = "24bit" ]] && : || logSilence "This terminal may not support 24-bit color (truecolor)"
-    fi
-
-    # Convert and output
-    local type=${value:+hsv}
-    type=${type:-hsl}
-    echo "$r $g $b $type" | awk -f "$_QFIG_LOC/staff/rgb2hslv.awk"
-    [ $show ] && printf " \e[48;2;$r;$g;${b}m  \e[0m\n" || printf "\n"
-}
-
-function hslv2rgb() { #? convert HSL(floats) or HSV(floats) to RGB(integers). -s to show the rgb color
-    local show value
-    OPTIND=1
-    while getopts "hsv" opt; do
-        case $opt in
-            h)
-              logInfo "Usage: hslv2rgb \$hue \$saturation \$lightness/\$value.\n  \nFlags:\n"
-              printf "    %-5s%s\n" "h" "Print this help message"
-              printf "    %-5s%s\n" "s" "Show the color in a 24-bit color terminal"
-              printf "    %-5s%s\n" "v" "Specify the input as HSV"
-              printf "\e[0m\n"
-              return 0;;
-            s) show=1;;
             v) value=1;;
             \?) logError "Invalid option: -$OPTARG"; return 1;;
         esac
@@ -149,13 +122,57 @@ function hslv2rgb() { #? convert HSL(floats) or HSV(floats) to RGB(integers). -s
     local rgb=$(echo "$h $s $lv $type" | awk -f "$_QFIG_LOC/staff/hslv2rgb.awk")
 
     # Output result
-    if [ "$show" ]; then
-        toArray $rgb && declare -a rgb1=("${_TEMP[@]}")
-        local ab=$(getArrayBase)
-        local R=${rgb1[$ab]} G=${rgb1[$((ab + 1))]} B=${rgb1[$((ab + 2))]}
-        printf "$rgb \e[48;2;$R;$G;${B}m  \e[0m\n"
+    toArray $rgb && declare -a rgb1=("${_TEMP[@]}")
+    local ab=$(getArrayBase)
+    local R=${rgb1[$ab]} G=${rgb1[$((ab + 1))]} B=${rgb1[$((ab + 2))]}
+    if [ "$type" = "hsl" ]; then
+        rgb -l $R $G $B
     else
-        echo "$rgb"
+        rgb -v $R $G $B
+    fi
+}
+function rgbPct() { #? convert RGB in percentage to RGB in decimal. Usage: rgbPct 39.21 78.43 100; rgbPct 39.21% 78.43% 100%;
+    local r=$1 g=$2 b=$3 invalidComps=()
+    r=$(+convertPct2Dec $r)
+    if [ $? -ne 0 ]; then
+        invalidComps+=("Red")
+    fi
+    g=$(+convertPct2Dec $g)
+    if [ $? -ne 0 ]; then
+        invalidComps+=("Green")
+    fi
+    b=$(+convertPct2Dec $b)
+    if [ $? -ne 0 ]; then
+        invalidComps+=("Blue")
+    fi
+    local invalidCompsStr=$(concat "-, -" "${invalidComps[@]}")
+    if [ ${#invalidComps[@]} -gt 0 ]; then
+        logError "Invalid input ! $invalidCompsStr should be a number between 0 and 100." && return 1
+    fi
+    rgb -p $r $g $b
+}
+
+function hsl() { #? convert HSL(floats) to RGB(integers). Usage: hsl 100 50 50;
+    +convertHslv2Rgb $1 $2 $3
+}
+
+function hsv() { #? convert HSV(floats) to RGB(integers). Usage: hsv 100 50 50;
+    +convertHslv2Rgb -v $1 $2 $3
+}
+
+function +convertPct2Dec() { #x
+    local pct=$1
+    if [[ $1 =~ ^[0-9]+(\.[0-9]+)?(%)?$ ]]; then
+        if [[ $1 =~ .*%$ ]]; then
+            pct=${pct%?}
+        fi
+        if (( $(echo "$pct > 100" | bc -l) )) || (( $(echo "$pct < 0" | bc -l) )); then
+            return 1
+        fi
+        pct=$(echo "scale=4; $pct / 100 * 255" | bc | awk '{print int($0 + 0.5)}')
+        echo $pct
+    else
+        return 1
     fi
 }
 
