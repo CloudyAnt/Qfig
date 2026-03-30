4adc7bcd49d76d29d264761cb1199edada91f286
Cloudy <iiim.cloudy@gmail.com>
2026-03-30 14:57:03 +0800

    Chai-M3MBP feat: add tscompile command

--- command/typescriptCommands.sh
+++ command/typescriptCommands.sh
@@ -1,4 +1,20 @@
-function tsrun() {
-    if ! +base:checkParams "FileName" "$1"; then return 1; fi
-    ./node_modules/typescript/bin/tsc "$1.ts" && node "$1.js"
+function +ts:tscCheck() { #x
+    if [ ! -f "./node_modules/typescript/bin/tsc" ]; then
+        logError "TypeScript is not installed. Run 'npm install typescript' first."
+        return 1
+    fi
+}
+
+function tsrun() { #? compile & run a ts file
+    if ! +base:checkParams "FileName" "$1" || ! +ts:tscCheck; then return 1; fi
+    local filename="$1"
+    [[ "$filename" =~ \.ts$ ]] && filename="${filename%.ts}"
+    ./node_modules/typescript/bin/tsc "${filename}.ts" && node "${filename}.js"
+}
+
+function tscompile() { #? compile a ts file
+    if ! +base:checkParams "FileName" "$1" || ! +ts:tscCheck; then return 1; fi
+    local filename="$1"
+    [[ "$filename" =~ \.ts$ ]] && filename="${filename%.ts}"
+    ./node_modules/typescript/bin/tsc "${filename}.ts"
 }
